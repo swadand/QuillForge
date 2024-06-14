4ac01d6e8ef5c982d634f80c58b8438f2e94fd4c
swadand <lwinseverytime@gmail.com>
2024-06-14 18:14:21 +0530

    Docker deployment script edited

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,4 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/views/common/header.php',
-                'resources/views/common/footer.php',
-            ],
-            refresh: true,
-        }),
-    ],
-    assetsInclude: ["**/*.php"]
 });
